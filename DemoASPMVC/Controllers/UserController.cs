f941491cf73ab0ae2d80e52ac4f4505965f65f8f
stevebstorm <steve.lorent@gmail.Com>
2023-09-28 16:43:25 +0200

    login Ok

--- DemoASPMVC/Controllers/UserController.cs
+++ DemoASPMVC/Controllers/UserController.cs
@@ -1,5 +1,6 @@
 ﻿using DemoASPMVC.Models.ViewModel;
 using DemoASPMVC_DAL.Interface;
+using DemoASPMVC_DAL.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DemoASPMVC.Controllers
@@ -26,18 +27,41 @@ namespace DemoASPMVC.Controllers
         [HttpPost]
         public IActionResult Register(UserRegisterForm u)
         {
-            if(!ModelState.IsValid)
+            if (!ModelState.IsValid)
             {
                 return View(u);
             }
 
-            if(_userService.Register(u.Email, u.Password, u.Nickname)) 
+            if (_userService.Register(u.Email, u.Password, u.Nickname))
             {
-                return RedirectToAction("Index", "Game") ;
+                return RedirectToAction("Index", "Game");
             }
 
             return View();
 
         }
+
+        public IActionResult Login()
+        {
+            ViewData["toto"] = DateTime.Now;
+            TempData["bidule"] = "jean maurice";
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(UserLoginForm u)
+        {
+            try
+            {
+                User connectedUser = _userService.Login(u.Email, u.Password);
+                return Ok(connectedUser);
+            }
+            catch (Exception ex)
+            {
+                ViewBag.Error = ex.Message;
+                return View();
+            }
+        }
+
     }
 }
